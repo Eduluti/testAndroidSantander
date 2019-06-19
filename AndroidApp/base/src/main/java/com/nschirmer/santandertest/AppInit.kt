ea909d12a0c97186921d8a18cb6a23793cb9631f
Nicolas Schirmer <nschirmer.dev@gmail.com>
2019-06-19 03:13:06 -0300

    Removing onCreate override from AppInit because there is no need now

--- AndroidApp/base/src/main/java/com/nschirmer/santandertest/AppInit.kt
+++ AndroidApp/base/src/main/java/com/nschirmer/santandertest/AppInit.kt
@@ -2,10 +2,4 @@ package com.nschirmer.santandertest
 
 import com.google.android.play.core.splitcompat.SplitCompatApplication
 
-class AppInit : SplitCompatApplication() {
-
-    override fun onCreate() {
-        super.onCreate()
-    }
-
-}
+class AppInit : SplitCompatApplication()
